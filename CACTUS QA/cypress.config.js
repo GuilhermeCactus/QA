5a9cf2eeca916274a82ba4428e4f096862eeaf6c
Guilherme Hernandez Batista <guilherme.batista@cactusgaming.net>
2025-09-02 15:04:50 -0300

    Atualizando projeto

--- CACTUS QA/cypress.config.js
+++ CACTUS QA/cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config
     },
+    // opcional: caminho para o Chrome se for personalizado
+    chromeWebSecurity: false
   },
-});
+})
